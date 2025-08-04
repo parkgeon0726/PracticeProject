10c6339d260a95dbc9c924699b86a4aab2cdb817
parkgeon <hit102412@naver.com>
2025-08-05 01:50:13 +0900

    Revert "📝 Swagger API 문서화 설정 (#9)"
    This reverts commit 0f54ad233c398636a00af132d598532c49d4e667.

--- practice_backend/src/modules/courses/courses.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CourseController } from './controllers/course/course.controller';
-import { CourseService } from './services/course/course.service';
-
-@Module({
-  controllers: [CourseController],
-  providers: [CourseService]
-})
-export class CoursesModule {}
